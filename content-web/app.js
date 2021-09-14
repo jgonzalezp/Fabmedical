35f64301b0294f6ffb23715b94d45ead8846d2aa
Jorge Gonzalez <george.alex1983@gmail.com>
2021-09-14 15:52:09 +0000

    Setup Environment Variables

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
-const contentApiUrl = process.env.CONTENT_API_URL || "http://20.106.161.52:3001";
+const contentApiUrl = process.env.CONTENT_API_URL || "http://168.61.21.17:3001";
 
 
 function getSessions(cb) {
